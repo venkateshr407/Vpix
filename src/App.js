645791d686a7d7f2c42bd514bacbb9147543c4f2
venkateshr407 <venkateshr590@gmail.com>
2020-07-21 11:09:44 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ImageList from './Component/ImageList';
+import DefaultPage from './Component/DefaultPage';
+import SearchBar from './Component/SearchBar';
+import Header from './Header';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+class App extends React.Component{
+
+  state = {images: [] };
+
+   onSearchSubmit = async (team) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos',{
+      params:{ query: team },
+      headers:{
+        Authorization:'Client-ID nq563ur4SWj9HTF0bhi9D8LU9Fe4eKM_aQVJ6ZsIbgE'
+      }
+      
+    }
+    
+    );
+      this.setState({images:response.data.results});
+    
+  }
+  
+  
+
+
+
+  render(){
+    return(
+      <Router>
+        <div>    
+          <Route exact path="/" component={Header}/>  
+          <Route exact path="/" component={DefaultPage}/>
+        </div>
+        <Route  path="/search">     
+          <div>
+            <Header/>
+            <SearchBar onSubmit={this.onSearchSubmit} />   
+             {/* {this.state.images.length}  */}
+            <ImageList images={this.state.images} />
+            
+          </div>
+        </Route>
+      </Router>
+    );
+  }
+}
+export default App;
---
+++ src/Component/SearchBar.js
@@ -0,0 +1,105 @@
+import React from "react";
+import styled from "styled-components";
+import { Form, FormControl } from "react-bootstrap";
+import image from "../Images/search.jpg";
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  position: relative;
+  background: url(${image}) no-repeat fixed;
+  background-size: cover;
+
+  .landing-section {
+    background-size: cover;
+    z-index: -1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    margin: 1rem;
+  }
+  .input {
+    margin: 1.5rem 0 0 0;
+    color: black;
+    width: 100%;
+  }
+  .input::placeholder {
+    color: black;
+  }
+`;
+
+const Text = styled.p`
+  color: white;
+  font-size: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 1rem 0rem 1rem;
+`;
+const Paragraph = styled.p`
+  color: white;
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 1rem 0rem 1rem;
+`;
+
+const Scroll = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 5rem;
+  color: #fff;
+  background-color: #0000;
+`;
+const SearchResult = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 2rem;
+  text-transform: uppercase;
+  color: #fff;
+  background-color: rgb(14, 13, 13);
+`;
+
+class SearchBar extends React.Component {
+  state = { team: "" };
+  state = { name: "Scroll Down" };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.team);
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <Container className="landing-section">
+          <Form className="form bg-transparent" onSubmit={this.onFormSubmit}>
+            <Text className="bg-transparent">vpix</Text>
+            <Paragraph className="para bg-transparent">
+              Powered by : Unsplash API
+            </Paragraph>
+            <div>
+              <FormControl
+                className="input"
+                style={{ color: "black" }}
+                type="text"
+                value={this.state.team}
+                placeholder="Search photos"
+                onChange={(e) => this.setState({ team: e.target.value })}
+              />
+            </div>
+            <Scroll>{this.state.name}</Scroll>
+          </Form>
+        </Container>
+        <SearchResult>{this.state.team}</SearchResult>
+      </React.Fragment>
+    );
+  }
+}
+export default SearchBar;
